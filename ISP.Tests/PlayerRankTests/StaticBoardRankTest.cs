a4706afd5457640a902ecad390e62c124d1656a9
DanialMohebbi <mohebbis@DanialMohebbis-MacBook-Pro.local>
2025-08-06 02:07:34 +0200

    Add RankTests

--- ISP.Tests/PlayerRankTests/StaticBoardRankTest.cs
+++ ISP.Tests/PlayerRankTests/StaticBoardRankTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ConsoleApp2;
 using ConsoleApp2.Extensions;
 using ConsoleApp2.Player;
@@ -7,7 +8,9 @@ namespace ISP.Tests.PlayerRankTests;
 
 public class StaticBoardRankTest
 {
-    public static IEnumerable<object[]> BoardRankTestData => new List<object[]>
+    private static int PlayerNotPlaying = -1;
+    private static int PlayerNotPlayingRank = 0;
+    public static IEnumerable<object[]> StaticBoardRankTestData => new List<object[]>
     {
         new object[] { new int[] { 0, 1, 2, 3 }, new int[] { 4, 3, 2, 1 } },
         new object[] { new int[] { 5, 1, 2, 10 }, new int[] { 2, 4, 3, 1 } },
@@ -17,13 +20,41 @@ public class StaticBoardRankTest
         new object[] { new int[] { 33, 34, 22, 1 }, new int[] { 2, 1, 3, 4 } },
 
     };
+    
     [Theory]
-    [MemberData(nameof(BoardRankTestData))]
+    [MemberData(nameof(StaticBoardRankTestData))]
     public void StaticBoardRank_ShouldReturnCorrectValue(int[] playerSquares, int[] ExpectedRanks)
     {
         GameState gameState = new GameState(NumberOfPlayers : 4);
         for (int i = 0; i < 4; i++)
             gameState.Board[playerSquares[i]].Player = gameState.Players[i];
+        
+        GameStateExtension.UpdateRank(gameState);
+        List<int> playersRank = new();
+        foreach (PlayerBase p in gameState.Players)
+            playersRank.Add(p.Rank);
+        
+        Assert.Equal(ExpectedRanks, playersRank.ToArray());
+    }
+
+    
+    public static IEnumerable<object[]> NotAllPlayersRankTestData => new List<object[]>
+    {
+        new object[] { new int[] { PlayerNotPlaying, PlayerNotPlaying, 10, 3 }, new int[] { PlayerNotPlayingRank, PlayerNotPlayingRank, 1, 2 } },
+        new object[] { new int[] { PlayerNotPlaying, PlayerNotPlaying, PlayerNotPlaying, PlayerNotPlaying }, new int[] { PlayerNotPlayingRank, PlayerNotPlayingRank, PlayerNotPlayingRank, PlayerNotPlayingRank } },
+        new object[] { new int[] { 2, 1, 3, PlayerNotPlaying }, new int[] { 2, 3, 1, PlayerNotPlayingRank } },
+        new object[]{ new int[] {PlayerNotPlaying, 31,PlayerNotPlaying, PlayerNotPlaying}, new int[] { PlayerNotPlayingRank, 1 , PlayerNotPlayingRank, PlayerNotPlayingRank } },
+        new object[]{ new int[] {PlayerNotPlaying, 31,21, PlayerNotPlaying}, new int[] { PlayerNotPlayingRank, 1 , 2, PlayerNotPlayingRank } },
+        new object[]{ new int[] {21, PlayerNotPlaying,PlayerNotPlaying, 31}, new int[] { 2, PlayerNotPlayingRank , PlayerNotPlayingRank, 1 } }
+    };
+    [Theory]
+    [MemberData(nameof(NotAllPlayersRankTestData))]
+    public void NotAllPlayersRank_ShouldReturnCorrectValue(int[] playerSquares, int[] ExpectedRanks)
+    {
+        GameState gameState = new GameState(NumberOfPlayers : 4);
+        for (int i = 0; i < 4; i++)
+            if (playerSquares[i] != PlayerNotPlaying)
+                gameState.Board[playerSquares[i]].Player = gameState.Players[i];
         
         GameStateExtension.UpdateRank(gameState);
         List<int> playersRank = new();
